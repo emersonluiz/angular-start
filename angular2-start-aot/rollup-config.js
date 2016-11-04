718f23a33c8510c4d5f9cc19858ecf6dcfbbd122
Emerson Luiz de Castro <erms.castro@gmail.com>
2016-11-03 23:51:27 -0200

    Create a start Angular 2

---
+++ angular2-start-aot/rollup-config.js
@@ -0,0 +1,18 @@
+import rollup      from 'rollup'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import commonjs    from 'rollup-plugin-commonjs';
+import uglify      from 'rollup-plugin-uglify'
+export default {
+  entry: 'app/main.js',
+  dest: 'aot/dist/build.js',
+  sourceMap: true,
+  sourceMapFile: 'aot/dist/build.js.map',
+  format: 'iife',
+  plugins: [
+      nodeResolve({jsnext: true, module: true}),
+      commonjs({
+        include: 'node_modules/rxjs/**',
+      }),
+      uglify()
+  ]
+}
